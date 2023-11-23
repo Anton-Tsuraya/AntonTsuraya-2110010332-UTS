c60a8c1763e816d99e2dd470514da6215787abbb
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-11-24 01:24:18 +0800

    Commit uts!

---
+++ src/GUI/Buku.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author antonid
+ */
+public class Buku {
+    int id;
+    String nama, telepon, kelamin, alamat;
+
+    public Buku(int id, String nama, String telepon, String kelamin, String alamat) {
+        this.id = id;
+        this.nama = nama;
+        this.telepon = telepon;
+        this.kelamin = kelamin;
+        this.alamat = alamat;
+    }
+
+    public Buku() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getKelamin() {
+        return kelamin;
+    }
+
+    public void setKelamin(String kelamin) {
+        this.kelamin = kelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+
+}
